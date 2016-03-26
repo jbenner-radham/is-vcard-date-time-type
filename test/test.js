93133fab9001b259f09276636ee22295ba646b3b
James Benner <james.benner@gmail.com>
2016-03-25 21:09:55 -0400

    Added `should verify that "date-and-or-time" is a valid type` spec.

--- test/test.js
+++ test/test.js
@@ -4,7 +4,11 @@ let expect              = require('chai').expect;
 let isVcardDateTimeType = require('../lib/is-vcard-date-time-type');
 
 describe('Is vCard Date/Time Type', () => {
-    it('should verify that "date" is a valid property', () => {
+    it('should verify that "date" is a valid type', () => {
         expect(isVcardDateTimeType('date')).to.equal(true);
+    });
+
+    it('should verify that "date-and-or-time" is a valid type', () => {
+        expect(isVcardDateTimeType('date-and-or-time')).to.equal(true);
     });
 });
